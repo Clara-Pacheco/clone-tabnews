9f7b5488e6ed42b456f88559a033ed496fc3de9e
Clara Pacheco <clarapacheco27@gmail.com>
2023-12-01 20:10:55 +0000

    Page formatted with prettier

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home(){
-    return(
-        <h1>You are my entire world, my darling child! ❤❤❤❤</h1>
-    ) 
+function Home() {
+  return <h1>You are my entire world, my darling child! ❤❤❤❤</h1>;
 }
 
-export default Home
+export default Home;
